5679cc4d4499e247c9ab46b61633f383649d9fae
Olivier <olivier.elophe@gmail.com>
2020-12-07 12:18:52 +0100

    Programme de base prêt à être completé

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # SNT-Narusturm
 
 Dans la cadre du cours de SNT nous allons créer un mini début de mini jeu en Python.
+
+Des tutos :
+https://pub.phyks.me/sdz/sdz/interface-graphique-pygame-pour-python.html
+https://zestedesavoir.com/tutoriels/846/pygame-pour-les-zesteurs/1381_a-la-decouverte-de-pygame/creer-une-simple-fenetre-personnalisable/
+
+Documentation :
+https://www.pygame.org/docs/ref/transform.html#pygame.transform.scale
--- narutsturm.py
+++ narutsturm.py
@@ -1,19 +1,29 @@
 ###########################################
-#              SNT M. Elophe              #
+#           SNT - M. Elophe - 2020        #
 ###########################################
 
 # Ici on va importer les modules que nous allons utiliser.
+# C'est comme le from robot import * de France IOI
 import pygame
 from pygame.locals import *
-
 
 # Ouverture de la fenêtre Pygame
 TAILLE_FENETRE = (640, 480)
 fenetre = pygame.display.set_mode(TAILLE_FENETRE)
+# On peut ajouter un titre à la fenêtre.
+pygame.display.set_caption("Narusturm !")
+
+# Couleur de fond au format RGB : (Rouge, Vert, Bleu)
+# En l'occurence, ici, j'ai jeté mon dévolu sur un petit violet pâle pas trop agrgessif.
+couleur = (92, 107, 192)
+fenetre.fill(couleur)
 
 
 
-# Boucle de jeu
+# Il faut penser à rafraichir l'écran ! (Surtout en été)
+pygame.display.flip()
+
+# Boucle de jeu, "infinie", parce que le jeu doit continuer tant que je ne l'arrête pas
 continuer = 1
 while continuer:
 	continuer = int(input())
